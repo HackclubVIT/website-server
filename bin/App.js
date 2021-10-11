4e8c2ffab7964f1c728a2f5ffd1a4d1477739f8a
porridgewithraisins <santhanambr2002@gmail.com>
2021-10-12 01:19:07 +0530

    add npm scripts, linting, formatting. Also added helmet as an express middleware

--- bin/App.js
+++ bin/App.js
@@ -5,11 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
 const express_1 = __importDefault(require("express"));
+const helmet_1 = __importDefault(require("helmet"));
 const ServerError_1 = require("./utils/ServerError");
 const Router_1 = __importDefault(require("./routes/Router"));
 const Error_Controller_1 = require("./controllers/Error.Controller");
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
+app.use((0, helmet_1.default)());
 app.use(express_1.default.json({ limit: "15kb" }));
 app.use(express_1.default.urlencoded({ extended: false }));
 app.use("/api", Router_1.default);
--- bin/Config.js
+++ bin/Config.js
@@ -4,4 +4,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config({ path: './.env' });
+dotenv_1.default.config({ path: "./.env" });
+console.log("Read environment variables from rootDir/.env...");
